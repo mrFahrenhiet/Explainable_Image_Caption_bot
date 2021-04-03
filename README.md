f37ab8f2909cb1efcb76d7265b94c773414ca95a
Shreyansh Gupta <shreyansh.gupta1299@gmail.com>
2021-04-04 02:53:50 +0530

    Readme updated, captions.py argument parser added

--- README.md
+++ README.md
@@ -1,3 +1,56 @@
-## Find pre-trained model file (voab included) and jupyter notebook for this repository on this link
+# Explainable Image Captioning
 
-https://drive.google.com/drive/folders/1JMdBNklNe7-4VWiVTUNtTQQVGX4LGZGL?usp=sharing
+The goal of image captioning is to convert a given input image into a natural language description.
+In this appart from generating captions we will also show the regions where the model looks to generate
+a perticular word, through attention mechanism.<br>
+
+In this project I have implemented the [Show, Attend and Tell Paper](https://arxiv.org/abs/1502.03044). Using
+[Pytorch](https://github.com/pytorch/pytorch)
+deep learning library.
+
+
+## Overview
+In this project I have implemented the [Show, Attend and Tell Paper](https://arxiv.org/abs/1502.03044)
+This is not the current state-of-the-art, but is amazing <br>
+
+***This model learns where to look.***
+
+As you generate a caption, word by word, you can see the model's gaze shifting across the image.
+
+This is possible because of its Attention mechanism, which allows it to focus on the part of the image most relevant to the word it is going to utter next.
+
+Here are some captions generated on test images not seen during training or validation:
+
+![](./imgs/Slide1.JPG)
+
+![](./imgs/Slide2.JPG)
+
+![](./imgs/Slide3.JPG)
+
+![](./imgs/Slide4.JPG)
+
+![](./imgs/Slide5.JPG)
+
+## Model Description
+The model basically has 3 parts :-
+- Encoder: We encode the image in 49 x 2048 size vector
+- Attention: attention make use of 49 layers of encoded image to calculate context vectors
+  which are to be passed on to the decoder.
+- Decoder: Each LSTM cell in the decoder recieves the context vector and given that and 
+the previous word predicted the decoder tries to predict the next word of the caption.
+
+![](imgs/model_arch.jpeg)<br>
+*Credits: Show, Attend and Tell paper*
+
+## Dataset Description
+**TO DO**
+
+## Colab Notebooks and Datasets
+The colab notebooks used for training procedures and datasets used in the development
+of this project can be found [here](https://drive.google.com/drive/folders/1JMdBNklNe7-4VWiVTUNtTQQVGX4LGZGL?usp=sharing)
+
+
+## Pretrained Model
+If you do not want to train the model from scratch, you can use a pretrained model.
+You can download the pretrained model and vocab state_checkpoint file from
+[here](https://drive.google.com/file/d/1-AIlZp28kvn13sEGJpD1vJY10aBuBg2a/view?usp=sharing)
--- caption.py
+++ caption.py
@@ -4,28 +4,29 @@ from model import *
 from utils import *
 import torchvision.transforms as T
 from PIL import Image
+import argparse
 
 
 # Will only work for batch size 1
-def get_bleu_score(dataset, img, target):
+def get_bleu_score(dataset, img, target, model, vocab=None):
     features = model.EncoderCNN(img[0:1].to(device))
-    caps, alphas = model.DecoderLSTM.gen_captions(features, vocab=dataset.vocab)
+    caps, alphas = model.DecoderLSTM.gen_captions(features, vocab=vocab)
     target_ = [[dataset.vocab.itos[i] for i in target[0].tolist() if dataset.vocab.itos[i] not in "<SOS>"]]
     return sentence_bleu(target_, caps)
 
 
-def get_batched_bleu(dataloader_eval, dataset):
+def get_batched_bleu(dataloader_eval, dataset, model, vocab=None):
     bleu = 0
     for batch in tqdm(dataloader_eval, total=len(dataloader_eval)):
         img, cap = batch
         img, cap = img.to(device), cap.to(device)
-        bleu += get_bleu_score(dataset, img, cap)
+        bleu += get_bleu_score(dataset, img, cap, model, vocab)
 
     bleu /= len(dataloader_eval)
     print("\nBlue Score: ", bleu * 100)
 
 
-def get_caps_from(features_tensors):
+def get_caps_from(features_tensors, model, vocab=None):
     model.eval()
     with torch.no_grad():
         features = model.EncoderCNN(features_tensors[0:1].to(device))
@@ -53,27 +54,25 @@ def plot_attention(img, target, attention_plot):
     plt.show()
 
 
-def plot_caption_with_attention(img_pth, transforms_=None):
+def plot_caption_with_attention(img_pth, model, transforms_=None, vocab=None):
     img = Image.open(img_pth)
     img = transforms_(img)
     img.unsqueeze_(0)
-    caps, attention = get_caps_from(img)
+    caps, attention = get_caps_from(img, model, vocab)
     plot_attention(img[0], caps, attention)
 
 
-if __name__ == "__main__":
-    state_checkpoint = torch.load("/content/attention_model_state.pth", map_location=device)  # change paths
-
+def main(arguments):
+    state_checkpoint = torch.load(arguments.state_chechpoint, map_location=device)  # change paths
     # model params
     vocab = state_checkpoint['vocab']
-    embed_size = 300
+    embed_size = arguments.embed_size
     embed_wts = None
     vocab_size = state_checkpoint['vocab_size']
-    attention_dim = 256
-    encoder_dim = 2048
-    decoder_dim = 512
-    fc_dims = 256
-    learning_rate = 5e-4
+    attention_dim = arguments.attention_dim
+    encoder_dim = arguments.encoder_dim
+    decoder_dim = arguments.decoder_dim
+    fc_dims = arguments.fc_dims
 
     model = EncoderDecoder(embed_size,
                            vocab_size,
@@ -92,5 +91,18 @@ if __name__ == "__main__":
         T.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
     ])
 
-    img_path = ""
-    plot_caption_with_attention(img_path, transforms)
+    img_path = arguments.image
+    plot_caption_with_attention(img_path, model, transforms, vocab)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--image', type=str, required=True, help='input image for generating caption')
+    parser.add_argument('--state_checkpoint', type=str, required=True, help='path for state checkpoint')
+    parser.add_argument('--embed_size', type=int, default=300, help='dimension of word embedding vectors')
+    parser.add_argument('--attention_dim', type=int, default=256, help='dimension of attention layer')
+    parser.add_argument('--encoder_dim', type=int, default=2048, help='dimension of encoder layer')
+    parser.add_argument('--decoder_dim', type=int, default=512, help='dimension of decoder layer')
+    parser.add_argument('--fc_dims', type=int, default=256, help='dimension of fully connected layer')
+    args = parser.parse_args()
+    main(args)
